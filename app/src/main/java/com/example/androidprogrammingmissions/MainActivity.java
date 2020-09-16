d7172a212fb8b35b57c6c80eff89afd05c32a934
algosketch <sjy_9484@naver.com>
2020-09-17 04:32:31 +0900

    UI - 버튼 활성화

--- app/src/main/java/com/example/androidprogrammingmissions/MainActivity.java
+++ app/src/main/java/com/example/androidprogrammingmissions/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.androidprogrammingmissions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button verificationBtn;
+    Button settingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verificationBtn = findViewById(R.id.verificationBtn);
+        settingBtn = findViewById(R.id.settingBtn);
+
+        verificationBtn.setOnClickListener(new ClickEvent());
+        settingBtn.setOnClickListener(new ClickEvent());
+    }
+
+    class ClickEvent implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            showToast("" + "button clicked!");
+        }
+    }
+
+    void showToast(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
