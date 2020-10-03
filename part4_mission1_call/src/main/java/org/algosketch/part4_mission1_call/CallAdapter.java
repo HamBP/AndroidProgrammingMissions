e8aa98d9b5ae855ad67a92c35bad62d88dbf4db9
algosketch <sjy_9484@naver.com>
2020-10-04 01:59:00 +0900

    call_list_item.xml - ListView의 item을 위한 custom 레이아웃 구성

---
+++ part4_mission1_call/src/main/java/org/algosketch/part4_mission1_call/CallAdapter.java
@@ -0,0 +1,6 @@
+package org.algosketch.part4_mission1_call;
+
+import android.widget.ArrayAdapter;
+
+public class CallAdapter extends ArrayAdapter<CallVO> {
+}
---
+++ part4_mission1_call/src/main/java/org/algosketch/part4_mission1_call/CallVO.java
@@ -0,0 +1,9 @@
+package org.algosketch.part4_mission1_call;
+
+public class CallVO {
+    boolean photo;
+    String name;
+    String type;
+    String date;
+    String phone;
+}
