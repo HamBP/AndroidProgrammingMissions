b7d6b69b8027a5554adb21ed1b82a91a243cfc97
algosketch <sjy_9484@naver.com>
2020-10-04 10:35:27 +0900

    4.mission3 init

---
+++ part4_mission3_autocomplete/src/main/java/org/algosketch/part4_mission3_autocomplete/MainActivity.java
@@ -0,0 +1,14 @@
+package org.algosketch.part4_mission3_autocomplete;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
