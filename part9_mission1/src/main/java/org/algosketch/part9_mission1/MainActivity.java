851e7d30f90c3d0b1ace4f70e081f4c1fd09ed3c
algosketch <sjy_9484@naver.com>
2020-11-19 03:20:55 +0900

    아ㅏ아ㅏ아아아아아아앙 왜 안 돼!!!!!!!!!!!!!!!!!!!

--- part9_mission1/src/main/java/org/algosketch/part9_mission1/MainActivity.java
+++ part9_mission1/src/main/java/org/algosketch/part9_mission1/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import static java.lang.String.valueOf;
@@ -27,19 +30,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 오늘의 날씨
         todayMaxTemperature = findViewById(R.id.today_max_temperature);
         todayMinTemperature = findViewById(R.id.today_min_temperature);
         currentTemperature = findViewById(R.id.current_temperature);
 
-        queue= Volley.newRequestQueue(this);
-        JsonObjectRequest jsonRequest=new JsonObjectRequest(Request.Method.GET, "https://api.openweathermap.org/data/2.5/weather?q=seoul&mode=json&units=metric&appid=e7d7b4b3f40299f32bf2c14dcae22048",
+        queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonTodayRequest = new JsonObjectRequest(Request.Method.GET, "https://api.openweathermap.org/data/2.5/weather?q=seoul&mode=json&units=metric&appid=e7d7b4b3f40299f32bf2c14dcae22048",
                 null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    currentTemperature.setText(valueOf(response.getJSONObject("main").getDouble("temp")));
-                    todayMaxTemperature.setText(valueOf(response.getJSONObject("main").getDouble("temp_max")));
-                    todayMinTemperature.setText(valueOf(response.getJSONObject("main").getDouble("temp_min")));
+                    JSONObject main = response.getJSONObject("main");
+                    currentTemperature.setText(valueOf(main.getDouble("temp")));
+                    todayMaxTemperature.setText(valueOf(main.getDouble("temp_max")));
+                    todayMinTemperature.setText(valueOf(main.getDouble("temp_min")));
                 }catch (Exception e){
                     e.printStackTrace();
                 }
@@ -50,6 +55,32 @@ public class MainActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
-        queue.add(jsonRequest);
+        queue.add(jsonTodayRequest);
+
+
+        // 주간 날씨
+        JsonObjectRequest jsonWeekRequest = new JsonObjectRequest(Request.Method.GET, "https://api.openweathermap.org/data/2.5/forecast/daily?q=seoul&cnt=7&mode=json&units=metric&appid=e7d7b4b3f40299f32bf2c14dcae22048",
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String a = valueOf(response.getInt("cnt"));
+                    currentTemperature.setText(a);
+                    //JSONArray weathers = response.getJSONArray("list");
+                    //for (int i = 0; i < 5; ++i) {
+                    //    currentTemperature.setText(response.getString("cod"));
+                    //}
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        queue.add(jsonWeekRequest);
     }
 }
